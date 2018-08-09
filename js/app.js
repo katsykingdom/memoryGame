72f0bb99c4adde08a0e353b19fe768f8adf8fcb0
katsykingdom <eszter@teszter.com>
2018-08-09 22:29:42 +0200

    - Added card shuffle function - Added card show function

--- js/app.js
+++ js/app.js
@@ -1,38 +1,33 @@
 /*
- * Create a list that holds all of your cards
+ * Suffle cards
  */
 
+//Shuffle function from: https://jsfiddle.net/qEM8B/
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+var deck = document.querySelector('ul.deck');
+for (var i = deck.children.length; i >= 0; i--) {
+    deck.appendChild(deck.children[Math.random() * i | 0]);
 }
 
+console.log(deck);
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+const cards = document.getElementsByClassName("card");
+const openCards = document.getElementsByClassName("show");
+
+console.log(cards);
+console.log(openCards);
+
+// Flip cards on click
+
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function(event){
+    this.classList.add("open", "show");
+    console.log("A card was opened!");
+})};
+
+
+
+if (openCards.length === 2) {
+    console.log("2 cards opened!");
+};
